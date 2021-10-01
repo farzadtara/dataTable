38cc9c7b7e06dc41274042ec32c8fa40808ec374
farzad <farzad.tara@gmail.com>
2021-10-01 22:06:06 +0330

    clear and create nuxt config

---
+++ app/nuxt.config.js
@@ -0,0 +1,47 @@
+export default {
+    mode: 'universal',
+    
+    head :{
+        title : 'MEVN STACK',
+        meta:[
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' }
+        ],
+        link:[],
+    },
+
+    css: [],
+
+    plugins: [],
+
+    comments: true,
+
+    buildModule:[],
+
+    modules:[],
+
+    axios:{},
+
+    pwa: {
+        manifest: {
+          lang: 'en'
+        }
+      },
+
+
+    build: {
+        vendor: ['vuetify']
+      }
+    }
+    
+
+
+
+
+
+
+
+
+}
